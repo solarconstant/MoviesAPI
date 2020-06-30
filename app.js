07ea988d05e83071443abd131a66207a310fc052
solarconstant <ashishshubh001@gmail.com>
2020-06-30 22:59:20 +0530

    alpha

--- app.js
+++ app.js
@@ -87,8 +87,13 @@ app.post('/results.ejs', (req, res) =>
     
 });
 
-app.listen(3000, () =>
+let port = process.env.PORT;
+if(port == null || port == "")
 {
-    console.log("Server is up and running on port 3000."); 
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
 
