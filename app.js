432fd065953a8141399ce712e10f48afb187ea21
solarconstant <ashishshubh001@gmail.com>
2020-07-14 19:41:20 +0530

    frontend changed

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const https = require('https');
+const mongoose = require('mongoose');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/movieDB', {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
 
 var results_movie = [];
 var movie_posters = [];
@@ -30,6 +37,11 @@ app.get('/about', (req, res) =>
 app.get('/contact', (req, res) =>
 {
     res.render('contact.ejs');
+});
+
+app.get('/register', (req, res) =>
+{
+    res.render('register.ejs')
 });
 
 app.post('/results.ejs', (req, res) => 
